1e5603326acc99d4c06af6b2812105fe936187f6
Gaytri-Thakre <gaytrithakre8@gmail.com>
2024-04-05 19:20:23 +0530

    Converting normal js file to module

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 // const heading = React.createElement("h1",{id:"head"},"Hello World from React");
 // console.log(heading);
 // const root = ReactDOM.createRoot(document.getElementById("root"));
